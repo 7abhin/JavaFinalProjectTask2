dcf3cf14555a49b4f8bf136bbc25112a1a6eb0c2
abhin anil <abhingfx@gmail.com>
2026-05-13 17:55:20 +0530

    first commit

---
+++ src/main/java/pl/edu/vistula/finaljavatask/FinalJavaTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.finaljavatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalJavaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalJavaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/vistula/finaljavatask/FinalJavaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.finaljavatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalJavaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
